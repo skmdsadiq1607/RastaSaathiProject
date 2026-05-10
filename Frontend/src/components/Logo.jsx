0f72e00a6e07b53ea2912b44f7bf7d89bf51337a
skmdsadiq1607 <skmdsadiq1607@gmail.com>
2026-05-10 19:52:10 +0530

    🚀 Force-Update: High-visibility White Dove Logo and Elite Narrative Sections

--- Frontend/src/components/Logo.jsx
+++ Frontend/src/components/Logo.jsx
@@ -2,21 +2,16 @@ import React from 'react';
 
 const Logo = ({ size = 32 }) => {
   return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <svg width={size} height={size} viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-        {/* Deep Crimson Solid Heart */}
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: size, height: size }}>
+      <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path 
-          d="M50 85 C 50 85, 20 60, 20 40 C 20 30, 28 22, 38 22 C 45 22, 50 27, 50 27 C 50 27, 55 22, 62 22 C 72 22, 80 30, 80 40 C 80 60, 50 85, 50 85" 
-          fill="#ef4444" 
+          d="M21 5C21 5 18 2 13 2C8 2 3 5 3 5C3 5 2 11 7 13C12 15 15 13 15 13C15 13 16 18 12 21C16 21 21 17 21 11C21 5 21 5 21 5Z" 
+          fill="white" 
         />
-        {/* Solid White Pulse Line */}
         <path 
-          d="M32 45h8l5-15 8 30 5-15h10" 
-          fill="none" 
-          stroke="white" 
-          strokeWidth="5" 
-          strokeLinecap="round" 
-          strokeLinejoin="round" 
+          d="M7 13C5 15 3 21 3 21C7 21 9 15 7 13Z" 
+          fill="white" 
+          opacity="0.8"
         />
       </svg>
     </div>
